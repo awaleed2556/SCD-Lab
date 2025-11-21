ec2d3a3751d8b28ea983dad622aaace0b387ec42
Abdul Waleed <2312272@LAB.SZABIST.NET.PK>
2025-11-21 11:47:23 +0500

    Week 9

---
+++ Week 9/Models/DatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HEHE.Models.Entities;
+namespace HEHE.Models;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options) { }
+    public DbSet<Products> products { get; set; }
+}
---
+++ Week 9/Models/Entities/Products.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HEHE.Models.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+
+        [StringLength(200)]
+        public string ?Description { get; set; }
+
+         
+    }
+}
